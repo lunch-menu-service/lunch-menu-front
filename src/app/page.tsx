e5662812675f588833c8d040efdb1b91f8b6ba28
trustmitt <mainmirror14@gmail.com>
2023-06-06 03:00:10 +0900

    feat : 랜딩 페이지 구조 잡기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+import MenuCard from "@/components/main/MenuCard";
+
 export default function Home() {
-    return <div>test</div>;
+    return (
+        <div className="flex justify-center mt-[56px]">
+            <div>
+                <div>
+                    <div className="text-center mb-[8px]">점또몰은 &lt;점심 메뉴 또 몰라!&gt;의 줄임말로, 점심메뉴와 함께 가까운 식당을 추천해드립니다.</div>
+                    <div className="text-center">아래 메뉴를 한가지만 선택해주세요!</div>
+                </div>
+                <div className="grid grid-cols-3">
+                    <MenuCard />
+                    <MenuCard />
+                    <MenuCard />
+                    <MenuCard />
+                    <MenuCard />
+                    <MenuCard />
+                    <MenuCard />
+                    <MenuCard />
+                    <MenuCard />
+                </div>
+            </div>
+        </div>
+    );
 }
---
+++ src/components/main/MenuCard.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MenuCard() {
+    return <div className="flex justify-center items-center">MenuCard</div>;
+}
