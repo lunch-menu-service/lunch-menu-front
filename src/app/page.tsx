cda1113fdd4edc88df8f5976b389801f97ca493d
trustmitt <mainmirror14@gmail.com>
2023-06-06 03:19:31 +0900

    feat : 로컬 폰트 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
         <div className="flex justify-center mt-[56px]">
             <div>
                 <div>
-                    <div className="text-center mb-[8px]">점또몰은 &lt;점심 메뉴 또 몰라!&gt;의 줄임말로, 점심메뉴와 함께 가까운 식당을 추천해드립니다.</div>
-                    <div className="text-center">아래 메뉴를 한가지만 선택해주세요!</div>
+                    <div className="text-center mb-[8px] font-regular text-[24px]">점또몰은 &lt;점심 메뉴 또 몰라!&gt;의 줄임말로, 점심메뉴와 함께 가까운 식당을 추천해드립니다.</div>
+                    <div className="text-center font-bold text-[40px] mb-[12px]">아래 메뉴를 한가지만 선택해주세요!</div>
                 </div>
                 <div className="grid grid-cols-3">
                     <MenuCard />
