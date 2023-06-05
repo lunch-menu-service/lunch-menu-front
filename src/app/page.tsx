7d8a5b399b4d2dd707014015997910c28dadf5c3
trustmitt <mainmirror14@gmail.com>
2023-06-06 03:23:29 +0900

    feat : 랜딩 페이지 구조 설정 - 메뉴 카드 컴포넌트 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
             <div>
                 <div>
                     <div className="text-center mb-[8px] font-regular text-[24px]">점또몰은 &lt;점심 메뉴 또 몰라!&gt;의 줄임말로, 점심메뉴와 함께 가까운 식당을 추천해드립니다.</div>
-                    <div className="text-center font-bold text-[40px] mb-[12px]">아래 메뉴를 한가지만 선택해주세요!</div>
+                    <div className="text-center font-bold text-[40px] mb-[40px]">아래 메뉴를 한가지만 선택해주세요!</div>
                 </div>
-                <div className="grid grid-cols-3">
+                <div className="grid grid-cols-3 gap-[30px]">
                     <MenuCard />
                     <MenuCard />
                     <MenuCard />
--- src/components/main/MenuCard.tsx
+++ src/components/main/MenuCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function MenuCard() {
-    return <div className="flex justify-center items-center">MenuCard</div>;
+    return <div className="flex justify-center items-center w-[380px] h-[256px] bg-slate-400">MenuCard</div>;
 }
